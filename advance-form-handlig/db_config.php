692e78ddee319c2d8febd8e301acffad21360130
Co3code <countian3@gmail.com>
2025-12-05 21:51:00 +0800

    adding db

---
+++ advance-form-handlig/db_config.php
@@ -0,0 +1,17 @@
+<?php
+// DATABASE CONNECTION
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "learning_php";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+
+?>
